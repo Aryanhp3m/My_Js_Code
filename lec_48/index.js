f3281deb78e53823fab0f3aaa35ef0a1ca3a77d7
Aryanhp3m <132233726+Aryanhp3m@users.noreply.github.com>
2025-04-14 15:15:06 +0000

    in-built objects

---
+++ lec_48/index.js
@@ -0,0 +1,30 @@
+console.log("aryan");
+
+// math inbuilt object
+
+console.log(Math.PI);
+
+
+console.log(Math.max(60,30,24,600,712,89));
+
+
+console.log(Math.round(1.6));
+
+console.log(Math.floor(1.9));
+
+
+console.log(Math.ceil(1.2));
+
+
+console.log(Math.abs(-5));
+
+
+
+// date inbuilt object
+
+let curr = new Date();
+
+let date = new Date('June 20 1998 07:15');
+
+console.log( date );
+
